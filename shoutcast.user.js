c96554fae6e337fdaa76d386bb05679d95503b99
kjwon15 <kjwonmail@gmail.com>
2017-12-03 07:39:50 +0900

    Import

---
+++ shoutcast.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         Shoutcast get stream URL
+// @namespace    https://github.com/kjwon15/shoutcast-userscript
+// @version      1.0
+// @description  Copy streaming URL on shoutcast
+// @author       Kjwon15
+// @match        http://www.shoutcast.com/
+// @grant        GM_setClipboard
+// ==/UserScript==
+
+function getStreamUrl(stationId) {
+    let data = new FormData();
+    data.append('station', stationId);
+    return fetch('/Player/GetStreamUrl', {
+        method: 'POST',
+        body: data,
+    }).then(res => res.json());
+}
+
+function patchTable() {
+    let queryString = '#station-list tr[id] td.radio-name a';
+    let table = document.querySelector('#station-list');
+    table.addEventListener('click', async (event) => {
+        let target = event.target;
+        if (!target.matches(queryString)) {
+            return;
+        }
+        event.preventDefault();
+
+        let stationId = target.closest('tr').getAttribute('id');
+        let url = await getStreamUrl(stationId);
+        let elem = document.createElement('input');
+        elem.readOnly = true;
+        elem.value = url;
+        target.closest('td').appendChild(elem);
+        elem.select();
+        GM_setClipboard(url);
+    });
+}
+
+window.addEventListener('load', () => {
+    patchTable();
+});
